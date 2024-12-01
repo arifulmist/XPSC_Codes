55672c5b2625e04f0c3f13bf473c23c0da286856
Md Ariful Islam <154077378+arifulmist@users.noreply.github.com>
2024-12-01 23:48:10 +0600

    Add files via upload

---
+++ week6/day1/Dull_Operation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<n<<" "<<n-1<<"\n";
+	   // for(ll i=0;:i++)
+	   // {
+	   //     int x=(i|j);
+	   //    int y=(i^j);
+	   //     int xy=x*y;
+	   //     if(xy==n)
+	   //     {
+	   //         cout<<x<<" "<<y;
+	   //     }
+	    }
+	   // int i=0,j=n;
+	   // while(i<j)
+	   // {
+	   //     int x=(i|j);
+	   //     int y=(i^j);
+	   //     int xy=x*y;
+	   //     if(xy==n)
+	   //     {
+	   //         cout<<x<<" "<<y;
+	   //     }
+	   //     i++;
+	   //     j--;
+	   // }
+	    
+	}
+
+}
