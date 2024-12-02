0c2ab1f5641c57c90d4a43536ff4b3884efd4d44
Md Ariful Islam <154077378+arifulmist@users.noreply.github.com>
2024-12-02 23:34:37 +0600

    Add files via upload

---
+++ week6/day2/C_-_Little_Nikita.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ariful ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long
+#define vi vector<long long>
+#define sot(x) (x).begin(),(x).end()
+#define yes cout<<"Yes\n"
+#define no cout<<"No\n"
+#define line "\n"
+#define shesh return 0;
+using namespace std;
+int main()
+{
+   ariful
+   ll t,m,n,c=0,x,y,z,a,b,d=0;
+   cin>>t;
+   while(t--)
+   {
+    cin>>n>>m;
+    if(n>=m && (n % 2 == m % 2))
+    {
+      yes;
+    }
+    else{
+      no;
+    }
+   }
+  shesh
+}
