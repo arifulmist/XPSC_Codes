cd57e3d00e7543fa1c86a9e36a9881e43b31cb44
Md Ariful Islam <154077378+arifulmist@users.noreply.github.com>
2025-02-28 23:41:17 +0600

    Add files via upload

---
+++ week16/day3/A_-_We_Got_Everything_Covered.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ariful ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define lb lower_bound
+#define ub upper_bound
+#define vi vector<long long>
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+  int n,k;
+  cin>>n>>k;
+  char s;
+  for(int i=0;i<n;i++)
+  {
+for(int j=0;j<k;j++)
+{
+  cout<<(char)('a'+j);
+}
+  }
+  cout<<'\n';
+
+
+
+
+}
+
+
+
+}
